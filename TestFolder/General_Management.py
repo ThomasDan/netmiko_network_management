9971822a812b307dbee88bf9b77a3739ab6efbf8
Jacob Nyyssønen Henriksen <Henriksen.Jacob@gmail.com>
2022-02-15 14:48:35 +0100

    Minor changes to General Management

--- TestFolder/General_Management.py
+++ TestFolder/General_Management.py
@@ -4,14 +4,12 @@ def config_hostname():
     print('What do you want the hostname to be?')
     input_hostname = input()
     hostname_output = config_sender('hostname '+ input_hostname)
-    command_sender('show hostname')
     print('The hostname is: ' + input_hostname)
 
 def config_motd():
     print('What is the message of the day?')
     input_motd = input()
     motd_output = config_sender('banner motd #'+ input_motd +'#')
-    command_sender('show banner motd')
     print('The message of the day is #' + input_motd + '#')
 
 def show_ints():
